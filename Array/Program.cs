f421fb2b41463172f0d976b3980151d6e99ec4b4
DaynerPow <daynerpowtube@gmail.com>
2026-04-06 20:29:25 +0300

    Додано завдання 4 , і функції

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,137 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Array
 {
     internal class Program
     {
-        static void Task1()
+        static int Input(string text)
         {
-            Random rnd = new Random();
-
-            int[] numbers = new int[1000];
-
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    numbers[i] = rnd.Next(1, 10);
-                    Console.Write(numbers[i]);
-
-                        if (i < 1000 - 1)
-                        {
-                            Console.Write(", ");
-                        }
-                }
-
-
-            int[] count = new int[10];
-
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    count[numbers[i]]++;
-                }
-
-
-            int minCount = count[1];
-            int result = 1;
-
-                for (int i = 2; i <= 9; i++)
-                {
-                    if (count[i] < minCount)
-                    {
-                        minCount = count[i];
-                        result = i;
-                    }
-                }
-
-            Console.WriteLine("\n\nЧисло, яке найменше повторювалося: " + result);
-            Console.WriteLine("Кількість повторень: " + minCount);
+            Console.WriteLine(text);
+            return Convert.ToInt32(Console.ReadLine());
         }
 
-
-        static void Task2()
-        {
-            int size = input("Введіть розмір масиву: ");
-
-            int min = input("Введіть початок діапазону (Від): ");
-
-            int max = input("Введіть кінець діапазону (До): ");
-
-
-            int[] numbers = new int[size];
-            Random rnd = new Random();
-
-                for (int i = 0; i < size; i++)
-                {
-                    numbers[i] = rnd.Next(min, max + 1);
-                }
-
-            Console.WriteLine("\nЗгенерований масив: ");
-                for (int i = 0; i < size; ++i)
-                {
-                    Console.Write(numbers[i]);
-
-                        if (i < size - 1)
-                        {
-                            Console.Write(", ");
-                        }
-                }
-
-            HashSet<int> uniqueSet = new HashSet<int>();
-                foreach (int num in numbers)
-                {
-                    uniqueSet.Add(num);
-                }
-
-            int[] result = new int[uniqueSet.Count];
-            uniqueSet.CopyTo(result);
-
-            Console.WriteLine("\n\nМасив без дублікатів: ");
-                for (int i = 0; i < result.Length; ++i)
-                {
-
-                    Console.Write(result[i]);
-
-                        if (i < result.Length - 1)
-                        {
-                            Console.Write(", ");
-                        }
-                }
-        }
-
-
-        static void Task3()
+        static int[] GenerateArray(int size, int min, int max)
         {
             Random rnd = new Random();
+            int[] array = new int[size];
 
-            int size = input("Введіть довжину масиву:  \n");
-
-            int[] numbers = new int[size];
-
-            for (int i = 0;i < size; ++i)
+            for (int i = 0; i < size; i++)
             {
-                numbers[i] = rnd.Next(100, 901);
+                array[i] = rnd.Next(min, max + 1);
             }
 
-            Console.WriteLine("\nІндекси піків: ");
+            return array;
+        }
 
-            for( int i = 0; i < size; i++)
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+
+                if (i < array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintPeaks(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
             {
                 if (i == 0)
                 {
                     if (numbers[i] >= numbers[i + 1])
                     {
-                        Console.WriteLine(i + " ");
+                        Console.Write(i + " ");
                     }
                 }
-                else if (i == size -1)
+                else if (i == numbers.Length - 1)
                 {
                     if (numbers[i] >= numbers[i - 1])
                     {
-                        Console.WriteLine(i + " ");
+                        Console.Write(i + " ");
                     }
                 }
                 else
@@ -142,13 +65,159 @@ namespace Array
                     }
                 }
             }
+            Console.WriteLine();
+        }
+
+        static void Task1()
+        {
+            int[] numbers = GenerateArray(1000, 1, 9);
+
+            PrintArray(numbers);
+
+            int[] count = new int[10];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                count[numbers[i]]++;
+            }
+
+            int minCount = count[1];
+            int result = 1;
+
+            for (int i = 2; i <= 9; i++)
+            {
+                if (count[i] < minCount)
+                {
+                    minCount = count[i];
+                    result = i;
+                }
+            }
+
+            Console.WriteLine("\nЧисло, яке найменше повторювалося: " + result);
+            Console.WriteLine("Кількість повторень: " + minCount);
+        }
+
+        static void Task2()
+        {
+            try
+            {
+                int size = Input("Введіть розмір масиву:");
+                int min = Input("Введіть початок діапазону (Від):");
+                int max = Input("Введіть кінець діапазону (До):");
+
+                int[] numbers = GenerateArray(size, min, max);
+
+                Console.WriteLine("\nЗгенерований масив:");
+                PrintArray(numbers);
+
+                HashSet<int> uniqueSet = new HashSet<int>();
+
+                foreach (int num in numbers)
+                {
+                    uniqueSet.Add(num);
+                }
+
+                int[] result = new int[uniqueSet.Count];
+                uniqueSet.CopyTo(result);
+
+                Console.WriteLine("\nМасив без дублікатів:");
+                PrintArray(result);
+            }
+            catch
+            {
+                Console.WriteLine("!Невірний набір!");
+            }
+        }
+
+        static void Task3()
+        {
+            try
+            {
+                int size = Input("Введіть довжину масиву:");
+
+                int[] numbers = GenerateArray(size, 100, 900);
+
+                Console.WriteLine("\nМасив:");
+                PrintArray(numbers);
+
+                Console.WriteLine("\nІндекси піків:");
+                PrintPeaks(numbers);
+            }
+            catch
+            {
+                Console.WriteLine("!Невірний набір!");
+            }
         }
 
 
-        static int input(string text)
+        static void Task4()
         {
-            Console.WriteLine(text);
-            return Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Напишіть любі англійські слова:");
+                string words = Console.ReadLine().Trim();
+
+                if (words.Length == 0)
+                {
+                    Console.WriteLine("!Немає слів!");
+                    return;
+                }
+
+                int count = 1;
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i] == ' ')
+                    {
+                        count++;
+                    } 
+                }
+
+                Console.WriteLine("Кількість слів: " + count);
+
+                string checkedChars = "";
+
+                foreach (char c in words)
+                {
+                    bool alreadyChecked = false;
+
+                    foreach (char checkedC in checkedChars)
+                    {
+                        if (c == checkedC)
+                        {
+                            alreadyChecked = true;
+                            break;
+                        }
+                    }
+
+                    if (!alreadyChecked)
+                    {
+                        int charCount = 0;
+
+                        foreach (char target in words)
+                        {
+                            if (c == target)
+                            {
+                                charCount++;
+                            }
+                        }
+
+                        Console.WriteLine("Символ '" + c + "' зустрічається: " + charCount + " разів");
+
+                        checkedChars += c;
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("!Невірний набір!");
+            }
+        }
+
+
+        static void Task5()
+        {
+            GenerateArray(20, 10, 100);
         }
 
         static void Main(string[] args)
@@ -156,7 +225,7 @@ namespace Array
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
 
-            Console.WriteLine("Виберіть задачу (1 , 2 , 3 , 4 , 5):");
+            Console.WriteLine("Виберіть задачу (1, 2, 3, 4, 5):");
             string choice = Console.ReadLine();
 
             switch (choice)
@@ -172,8 +241,19 @@ namespace Array
                 case "3":
                     Task3();
                     break;
-            }
 
-        } 
+                case "4":
+                    Task4();
+                    break;
+
+                case "5":
+                    Task5();
+                    break;
+
+                default:
+                    Console.WriteLine("!Невірний набір!");
+                    break;
+            }
+        }
     }
 }
